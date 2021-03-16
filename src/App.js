acb7535d6010f2d5fae68b4610581745cb6cc52c
Chey Rose <cheyroseflammer@gmail.com>
2021-03-16 16:45:26 -0700

    stateless functional components built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { Component } from 'react'
 import './App.css'
+import Navbar from './layout/Navbar'
+import Users from './users/Users'
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Start</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
